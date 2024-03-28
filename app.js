d90257a3280eda3105536e115b0ce01743f430dd
张** <162709747+gokiburiSoldier@users.noreply.github.com>
2024-03-28 20:50:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var downloadButton = document.querySelector('#download');
 var download32 = document.getElementsByClassName('download')[0];
 
 downloadButton.innerHTML = os ? "[Windows 下载]" : "暂时不可下载";
+download32.innerHTML = os ?     "x86下载[推荐]"  : "暂时不可下载"
 
 if(os) {
-    download32.setAttribute('href','https://github.com/gokiburiSoldier/GS_repo_exe/releases/download/v1.14.514/-Beta-x86.exe');
+    download32.setAttribute('href','https://github.com/gokiburiSoldier/GS_repo_exe/releases/download/v1.14.514-II/cicha-download-v1-14-5-14.exe');
     downloadButton.setAttribute('href','https://github.com/gokiburiSoldier/GS_repo_exe/releases/download/v1.14.514/-Beta.exe');
 }
 
